5d2a71c4907475a01ac39e9ddea9e1789f9ecbf3
Mandar A <mandarashtekar@users.noreply.github.com>
2020-07-17 13:19:54 -0700

    Update index.js
    Bug Fix

--- js/index.js
+++ js/index.js
@@ -6,36 +6,11 @@
 
 'use strict';
 
-window.onload = () => {
+/*window.onload = () => {
     console.log("inside index.js - onload");
-
-    /*if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('./sw.js', { scope: '/' })
-          .then(function (registration){
-            console.log('Service worker registered successfully on scope: ' +registration.scope);
-          }).catch(function(e){
-            console.error('Error during service worker registration:', e);
-          });
-    }
-    self.addEventListener('install', function(event) {
-        console.log('Installed sw.js', event);
-    });
-
-    self.addEventListener('activate', function(event) {
-        console.log('Activated sw.js', event);
-    });
-    self.addEventListener('fetch', function(event){
-        console.log("Fetch - Requested event: " +event.request);
-    });*/
-    
-    Notification.requestPermission(result => {
-        if (result === 'granted') {
-          console.log("Thanks for giving me permissions");
-        }
-    });
 }
 
-/*$(document).ready(function(){
+$(document).ready(function(){
     console.log("inside index.js - document.ready");
 });*/
 
@@ -64,29 +39,6 @@ $("#join-conf").on("click", function(){
 
   // initialise("vve-tpmg-lab.kp.org", alias, bandwidth, name, "", source);
 });
-
-/* ************ Push Notification ************ */
-/*var butInstall = document.getElementById('butInstall');
-
-// Push Notifications for PWA
-butInstall.addEventListener('click', () => {
-  console.log("Button clicked");
-  showNotification('So nice to have you here!', 'Hey there!');
-});
-
-function showNotification(title, message) {
-  console.log("inside showNotification");
-    if ('Notification' in window) {
-      navigator.serviceWorker.ready.then(registration => {
-          registration.showNotification('This is a sample', {
-            //body: 'Buzz! Buzz!',
-            //tag: 'vibration-sample'
-            body: message,
-            tag: title
-          });
-      });
-  }
-}*/
 
 /*------------------- PERMISSIONS API - START -------------------*/
 /*const permissionsToRequest = {
